eebed4a7adaecd1c22f582f7a23b84a819fb6a80
Kyle <Kyle.fire@gmail.com>
2022-09-27 11:03:10 +0900

    first commit. added custom colors for tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        white: '#ffffff',
+        black: '#070724',
+        'gray-dark': '#38384F',
+        gray: '#838391',
+        'blue-light': '#419EBB',
+        yellow: '#EDA249',
+        purple: '#6F2ED6',
+        'orange-100': '#CD5120',
+        'orange-200': '#D14C32',
+        'orange-300': '#D83A3A',
+        green: '#1EC1A2',
+        blue: '#2D68F0',
+      },
+    },
+  },
+  plugins: [],
+};
