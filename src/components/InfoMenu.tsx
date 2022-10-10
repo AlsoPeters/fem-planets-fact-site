471927eacbdc8b2edea1260c0241f6c55d228334
Kyle <Kyle.fire@gmail.com>
2022-10-10 11:06:49 +0900

    set up infoMenu selection toggle

--- src/components/InfoMenu.tsx
+++ src/components/InfoMenu.tsx
@@ -1,31 +1,27 @@
-import React, { useState } from 'react';
-import classNames from 'classnames';
+import React from 'react';
 
 export default function InfoMenu({ setMenu, selected }: Props): JSX.Element {
-  const defaultStyle = 'border-2 translate-y-4';
-  const activeStyle = 'border-blue-light';
+  const defaultStyle = 'border-2 translate-y-4 opacity-0';
+  const activeStyle = 'border-2 border-blue-light translate-y-4';
 
   return (
     <div className='flex justify-between items-center px-6 py-4 border-b border-gray-dark font-spartan text-gray'>
-      <div>{selected}</div>
       <div onClick={() => setMenu('overview')}>
         OVERVIEW
         <div
-          className={classNames(defaultStyle, {
-            // See [activeStyle](global.css .activeStyle) for more info
-            activeStyle: selected === 'overview',
-          })}
+          className={selected === 'overview' ? activeStyle : defaultStyle}
         ></div>
       </div>
       <div onClick={() => setMenu('structure')}>
         STRUCTURE
-        <div className='border-2  border-blue-light translate-y-4'></div>
+        <div
+          className={selected === 'structure' ? activeStyle : defaultStyle}
+        ></div>
       </div>
-      <div>
+      <div onClick={() => setMenu('surface')}>
         SURFACE
         <div
-          onClick={() => setMenu('surface')}
-          className='border-2  border-blue-light translate-y-4'
+          className={selected === 'surface' ? activeStyle : defaultStyle}
         ></div>
       </div>
     </div>
